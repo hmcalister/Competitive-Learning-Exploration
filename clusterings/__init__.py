fa777e8cb0710529f432d21636f9364c28ad2a27
Hayden McAlister <mcaha814@student.otago.ac.nz>
2025-09-10 14:07:36 +1200

    Add base ClAM Clustering to init imports
    Add base ClAM Clustering to init imports

--- clusterings/__init__.py
+++ clusterings/__init__.py
@@ -1,6 +1,7 @@
 from .WinnerTakesAll import WinnerTakesAll
 from .FSCL import FSCL
 from .RPCL import RPCL
+from .ClAM import ClAMClustering
 
 
-__all__ = ["WinnerTakesAll", "FSCL", "RPCL"]
+__all__ = ["WinnerTakesAll", "FSCL", "RPCL", "ClAMClustering"]
