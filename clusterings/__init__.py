c4702f02b1db2a51cc87de28c7f75885dada21f2
Hayden McAlister <mcaha814@student.otago.ac.nz>
2025-09-10 15:58:55 +1200

    Add RegularizedClAM to init imports

--- clusterings/__init__.py
+++ clusterings/__init__.py
@@ -1,7 +1,7 @@
 from .WinnerTakesAll import WinnerTakesAll
 from .FSCL import FSCL
 from .RPCL import RPCL
-from .ClAM import ClAMClustering
+from .ClAM import ClAMClustering, RegularizedClAM
 
 
-__all__ = ["WinnerTakesAll", "FSCL", "RPCL", "ClAMClustering"]
+__all__ = ["WinnerTakesAll", "FSCL", "RPCL", "ClAMClustering", "RegularizedClAM"]
