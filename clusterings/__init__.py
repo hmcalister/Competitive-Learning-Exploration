70e338769724b8ce2ee3153a2c4b2a2fe641f9b3
Hayden McAlister <haydenmcalister49@gmail.com>
2025-09-11 23:42:35 +1200

    Add imports for ClAMTrainingCallback and ClusteringPerformanceHistoryCallback

--- clusterings/__init__.py
+++ clusterings/__init__.py
@@ -1,7 +1,15 @@
 from .WinnerTakesAll import WinnerTakesAll
 from .FSCL import FSCL
 from .RPCL import RPCL
-from .ClAM import ClAMClustering, RegularizedClAM
+from .ClAM import ClAMClustering, RegularizedClAM, ClAMTrainingCallback, ClusteringPerformanceHistoryCallback
 
 
-__all__ = ["WinnerTakesAll", "FSCL", "RPCL", "ClAMClustering", "RegularizedClAM"]
+__all__ = [
+    "WinnerTakesAll", 
+    "FSCL", 
+    "RPCL", 
+    "ClAMClustering", 
+    "RegularizedClAM", 
+    "ClAMTrainingCallback", 
+    "ClusteringPerformanceHistoryCallback"
+]
